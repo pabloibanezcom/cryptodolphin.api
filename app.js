27308b13402a6439984fa1fef74587556b5403f2
Pablo Ibanez <me@pabloibanez.com>
2017-02-08 15:23:52 +0000

    # This is a combination of 22 commits. # The first commit's message is: Logs
    # This is the 2nd commit message:
    
    Logs
    
    # This is the 3rd commit message:
    
    Logs
    
    # This is the 4th commit message:
    
    Logs
    
    # This is the 5th commit message:
    
    Logs
    
    # This is the 6th commit message:
    
    Logs
    
    # This is the 7th commit message:
    
    Logs
    
    # This is the 8th commit message:
    
    Logs
    
    # This is the 9th commit message:
    
    Logs
    
    # This is the 10th commit message:
    
    Logs
    
    # This is the 11th commit message:
    
    Logs
    
    # This is the 12th commit message:
    
    Logs
    
    # This is the 13th commit message:
    
    Logs
    
    # This is the 14th commit message:
    
    Logs
    
    # This is the 15th commit message:
    
    Logs
    
    # This is the 16th commit message:
    
    Logs
    
    # This is the 17th commit message:
    
    Logs
    
    # This is the 18th commit message:
    
    Logs
    
    # This is the 19th commit message:
    
    Logs
    
    # This is the 20th commit message:
    
    Logs
    
    # This is the 21st commit message:
    
    Logs
    
    # This is the 22nd commit message:
    
    Logs

--- app.js
+++ app.js
@@ -15,6 +15,23 @@ require('dotenv').load();
 // configuration ===============================================================
 mongoose.connect(process.env.MONGODB_URI); // connect to our database
 
+// CONNECTION EVENTS
+// When successfully connected
+mongoose.connection.on('connected', function () {  
+  console.log('Mongoose default connection open to ' + process.env.MONGODB_URI);
+}); 
+
+// If the connection throws an error
+mongoose.connection.on('error',function (err) {  
+  console.log('Mongoose default connection error: ' + err);
+}); 
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', function () {  
+  console.log('Mongoose default connection disconnected'); 
+});
+
+
 require('./config/passport')(passport); // pass passport for configuration
 
 var app = express();
--- config/passport.js
+++ config/passport.js
@@ -23,19 +23,40 @@ module.exports = function (passport) {
     },
         function (accessToken, refreshToken, profile, done) {
 
-            var user = {
+            var fbuser = {
                 'email': profile.emails[0].value,
                 'name': profile.name.givenName + ' ' + profile.name.familyName,
-                'id': profile.id,
+                'id': profile.id + '',
                 'token': accessToken
             }
 
-            User.find({ facebookId: user.id }, function (err, users) {
-                if(users.length > 0) {
-                    return done(null, users[0]);
-                } 
-                return done(null, false, {message: 'User not allowed'});
+            console.log('FB_USER', fbuser);
+
+            User.find({}, function (err, users) {
+                var authorizedUser = false;
+                users.forEach(function (user) {
+                    if (user._doc.facebookId == fbuser.id) {
+                        authorizedUser = user;
+                    }
+                });
+                return done(null, authorizedUser);
+                // if (isAuthorized) {
+                //     return done(null, user);
+                // } else {
+                //     return done(null, false, { message: 'User not allowed' });
+                // }
             });
+
+            // User.find({ facebookId: fbuser.id }, function (err, users) {
+            //     console.log('USERS: ', users);
+            //     console.log('ERROR: ', err);
+            //     if (users.length > 0) {
+            //         return done(null, users[0]);
+            //     } else {
+            //         return done(null, false, { message: 'User not allowed' });
+
+            //     }
+            // });
         }
     ));
 
